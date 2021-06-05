47e8b1410ade75b747427f538b19e707448d7f9f
charun umesh <charunumesh@gmail.com>
2021-06-05 22:47:34 +0530

    feature upgrade

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
+
+    def validate_username(self, check_username):
+        user = User.query.filter_by(username=check_username.data).first()
+        if user:
+            raise ValidationError('User Name is already registered..! Please try a different User Name')
+
+    def validate_email(self, check_email):
+        email = User.query.filter_by(username=check_email.data).first()
+        if email:
+            raise ValidationError('Provided Email address is already registered..! Please try a different Email Address')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:',  validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:',  validators=[DataRequired()])
+    submit = SubmitField(label='Sign In')
--- market/models.py
+++ market/models.py
@@ -1,14 +1,38 @@
-from market import db
+from market import db, bcrypt, login_manager
 from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), nullable=False, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owner')
+
+    @property
+    def pretier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'${str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+        else:
+            return f'${self.budget}'
+
+    @property
+    def password_hash(self):
+        return self.password_hash
+
+    @password_hash.setter
+    def password_hash(self, plain_text_password):
+        self.password = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        if bcrypt.check_password_hash(self.password, attempted_password):
+            return True
 
 
 class Item(db.Model):
